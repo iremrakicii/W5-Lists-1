857c218bd9557d2eeb8e11a0c7d39cc74a74bbcd
İrem Rakıcı <iremrakici53@hotmail.com>
2024-10-29 14:06:33 +0300

    Proje dosyası ekle.

---
+++ W5-Lists-1/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<string> gala = new List<string>();
+        gala.Add("1-Bülent Ersoy");
+        gala.Add("2-Ajda Pekkan");
+        gala.Add("3-Ebru Gündeş");
+        gala.Add("4-Hadise");
+        gala.Add("5-Hande Yener");
+        gala.Add("6-Tarkan");
+        gala.Add("7-Funda Arar");
+        gala.Add("8-Demet Akalın");
+
+        Console.WriteLine("Patika Plus Galasına Hoşgeldiniz!");
+        Console.WriteLine("**Davetliler**");
+        foreach (var kisiler in gala)
+        {
+            Console.WriteLine(kisiler);
+        }
+
+    }
+}
